ac6ef2310e5215e3bfe18a5e0a3803c0dea9726a
Jeff Clegg <jeffclegg@att.net>
2020-12-03 23:52:02 -0500

    fixed sql files

--- dataBase/seed.sql
+++ dataBase/seed.sql
@@ -4,23 +4,23 @@ INSERT INTO department
     (d_name)
 VALUES
     ('IT'),
-    ('Production')
+    ('Production'),
     ('Sales'),
     ('Engineering'),
     ('Finance'),
     ('Legal');
 
-INSERT INTO roles
+INSERT INTO role
     (title, salary, department_id)
 VALUES
     ('IT Manager', 65000, 1),
     ('IT Tech', 50000, 1),
     ('Production Manager', 50000, 2),
     ('Team Lead', 40000, 2),
-    ('Operator', 30000, 2),
+    ('Operator', 36000, 2),
     ('Sales Manager', 100000, 3),
     ('Sales Lead', 90000, 3),
-    ('Salesperson', 70000, 3);
+    ('Salesperson', 70000, 3),
     ('Eng. Manager', 110000 , 4),
     ('Software Engineer', 85000, 4),
     ('Lead Engineer', 100000, 4),
@@ -30,15 +30,15 @@ VALUES
     ('Lawyer', 190000, 6);
     
 
-INSERT INTO employees
+INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
     ('Joel', 'Iles', 1, NULL ),
     ('Theresa', 'Louise', 2, 1),
-    ('Mitch', 'Young', 3, NULL),
-    ('Duke', 'Johnson', 4, 3),
+    ('Bill', 'Billheimer', 3, NULL),
+    ('Garth', 'Brooks', 4, 3),
     ('Josh', 'Maholmes', 5, 3),
-    ('Leslie', 'Knope', 6, NULL),
+    ('Leslie', 'Swoop', 6, NULL),
     ('Ron', 'Swanson', 7, 6),
     ('April', 'Ludgate',8, 6),
     ('Donna', 'Awesome', 9, NULL),

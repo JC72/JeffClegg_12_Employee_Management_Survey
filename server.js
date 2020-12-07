fe32e8f1099e9fc8c7723f92b8b94f994bea9261
Jeff Clegg <jeffclegg@att.net>
2020-12-07 18:49:07 -0500

    updated sqlStart and server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const cTable = require('console.table');
 var mysql = require("mysql");
-const util = require("util");
+
+// calls the function to start mySql
 const connection = require("./lib/js/sqlStart");
+
+//These are needed so to call various files needed for the inquirer prompts and menu choices
+const mainMenu = require('./lib/js/mainMenuChoices')
+const prompts = require('./lib/js/prompts')
 
 // Program Logo requirements based off of package.json file created: 
 const logo = require('asciiart-logo');
@@ -12,14 +16,13 @@ console.log(logo(config).render());
 
 
 
-connection.connect((err) => {
-    if (err) {
-        console.log(err);
-        res.status(500);
-        return res.send("There was an error connecting to the database.");
-    } console.log("You're connected!");
+// Calls the Function to start application
+runSearch();
 
-    // Function for inquirer to prompt data
-    runSearch();
+//Function to generate the prompts and menu selections for the application
+function runSearch() {
+
+    
+}
 
 })
